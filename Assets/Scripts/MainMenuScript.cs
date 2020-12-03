5d6916ae8888ddf7d7221364b6061f6d97f0fbba
Dopeye <71829074+Dopeye@users.noreply.github.com>
2020-12-03 15:33:13 +0000

    MainMenu - UI

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour
+{
+    /*
+    public void GoToGarage()
+    {
+        SceneManager.LoadScene("Garage");
+    }
+
+    public void RacetrackSelection()
+    {
+        SceneManager.LoadScene("Racetracks");
+    }
+    */
+    public void ExitTheGame()
+    {
+        Debug.Log("QUIT THE GAME");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/RacetrackSelectionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacetrackSelectionScript : MonoBehaviour
+{
+/*
+    public void RacetrackSelection_1()
+    {
+
+    }
+
+    public void RacetrackSelection_2()
+    {
+
+    }
+    
+    public void RacetrackSelection_3()
+    {
+        
+    }
+
+    public void RacetrackSelection_3()
+    {
+        
+    }
+    */
+}
