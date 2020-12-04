08169d11379b65ad8662f7c8dc68574caf4ccaee
Dopeye <71829074+Dopeye@users.noreply.github.com>
2020-12-04 16:57:44 +0000

    UI Settings & Sound - Updated

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref = "BackgroundPref";
+    private static readonly string SFXPref = "SFXPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider, sfxSlider;
+    private float backgroundFloat, sfxFloat;
+
+    public AudioSource backgroundAudio;
+    public AudioSource[] sfxAudio;
+
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+        // First time setup for sound
+        // Set sound to default settings
+        if(firstPlayInt == 0)
+        {
+            // Set the values
+            backgroundFloat = .25f;
+            sfxFloat = .75f;
+            // Update the values in the game
+            backgroundSlider.value = backgroundFloat;
+            sfxSlider.value = sfxFloat;
+            // Save background and SFX sound settings for future run throughs
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetFloat(SFXPref, sfxFloat);
+            // Change FirstPlay value, as player has played before
+            PlayerPrefs.SetInt(FirstPlay, -1);
+            Debug.Log("FirstPlay");
+        }
+        else
+        {
+            // Storing previous sound values and applying them to the current game
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+            
+            sfxFloat = PlayerPrefs.GetFloat(SFXPref);
+            sfxSlider.value = sfxFloat;
+            Debug.Log("SecondPlay");
+        }
+    }
+
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+        PlayerPrefs.SetFloat(SFXPref, sfxSlider.value);
+    }
+
+    // save game whenver focus is lost (i.e. minimising app, closing app, etc)
+    void OnApplicationFocus(bool inFocus)
+    {
+        if(!inFocus)
+        {
+            SaveSoundSettings();
+        }
+    }
+
+    // Update volume of audio with new volume settings
+    public void UpdateSound()
+    {
+        
+        backgroundAudio.volume = backgroundSlider.value;
+        Debug.Log("Background Update");
+        // loop through list of audio samples and apply the new volume 
+        for (int i = 0; i < sfxAudio.Length; i++)
+        {
+            sfxAudio[i].volume = sfxSlider.value;
+            Debug.Log("sfx Update");
+        }
+    }
+}
--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
+    public TMP_Dropdown qualityDropdown;
+    
+    public Dropdown resolutionDropdown;
+    
     // Start is called before the first frame update
+    Resolution[] resolutions;
+    
+
     void Start()
     {
+        // Setting Quality dropdown value to current quality setting
+        int index = QualitySettings.GetQualityLevel();//qualityDropdown.value;
+        qualityDropdown.value =  index;
         
+        // Setting Resolution dropdown value to current screen setting
+        // Collect array of possible resolutions
+        resolutions = Screen.resolutions;
+        // clear list of resolutions within dropdown
+        resolutionDropdown.ClearOptions();
+        // create list of strings with our options
+        List<string> options = new List<string>();
+        Debug.Log(resolutions);
+        int currentResolutionIndex = 0;
+        // loop through each resolution to create our option list
+        for ( int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);            
+
+            if(resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        // add options list to resolution dropdown
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    // function to determine the sound Volume set by user
+    public void SetVolume(float volume)
     {
-        
+        audioMixer.SetFloat("Volume", volume);
+    }
+    // function to determine the Quality set by user
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    // function to determine the Fullscreen set by user
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
