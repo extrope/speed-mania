d3f3f426ad4adc869fa4bc51928e2d0936b84ebc
Augustas Tiknius <47028638+extrope@users.noreply.github.com>
2020-12-07 09:09:41 +0000

    Ayush Merge into Race scene

--- Assets/Scripts/FinishEvent.cs
+++ Assets/Scripts/FinishEvent.cs
@@ -2,19 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishEvent : MonoBehaviour
-{
-    public static FinishEvent instance;
-    GameManager manager;
-  
-   
-    void start()
-    {
-        manager = GameObject.Find("GameManager").GetComponent<GameManager>();
-    }
-    void OnTriggerEnter()
-    {
-       manager.Win();
-       TimerController.instance.EndTimer();
-    }
+public class FinishEvent : MonoBehaviour {
+	void OnTriggerEnter() {
+		Extensions.GetObject("TimeController").GetOnlyComponent<TimerController>().EndTimer();
+		Extensions.GetObject("GameManager").GetComponent<GameManager>().Win();
+	}
 }
